ae9ef6cd7650dc69bc3d12b749a8deadd2e3067f
AaronPhilips101 <75502410+AaronPhilips101@users.noreply.github.com>
2020-12-26 01:31:07 +0530

    Update usage.py

--- modules/usage.py
+++ modules/usage.py
@@ -64,7 +64,7 @@ async def usage(message):
                        f"     •  `{AppHours}`**h**  `{AppMinutes}`**m**  "
                        f"**|**  [`{AppPercentage}`**%**]"
                        "\n"
-                       " -> `Dyno hours quota remaining this month`:\n"
+                       " -> `Hours remaining till I die, in this month :/`:\n"
                        f"     •  `{hours}`**h**  `{minutes}`**m**  "
                        f"**|**  [`{percentage}`**%**]")
 
