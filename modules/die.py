5fae0f80a1e70f4bb5cc80b011fcdac877dd88ca
AaronPhilips101 <75502410+AaronPhilips101@users.noreply.github.com>
2021-01-02 23:09:04 +0530

    Added new Die module

---
+++ modules/die.py
@@ -0,0 +1,22 @@
+#Die Module for CyberDoge
+#Created By @itexpert120
+#Modifyed By @AaronPhilips101
+
+@client.on(events("coinflip ?(.*)"))
+async def handler(event):
+    if event.fwd_from:
+        return
+    x = random.randint(1, 6)
+await event.edit("Rolling Dice")
+time.sleep(1)
+
+await event.edit(f"The Die is: {x}")
+
+
+ENV.HELPER.update({
+    "die": "\
+.die``` [No Arguemts Present]\
+\nUsage: Roll a Dice.\
+"
+})
+```
